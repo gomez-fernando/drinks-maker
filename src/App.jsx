b6c6abf730ca0b9b2015f342140df036693febd3
gomez-fernando <ferdgomez@hotmail.es>
2023-01-01 11:03:29 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Container from 'react-bootstrap'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='py-5'>
+        <h1>Creador de Bebidas</h1>
+      </header>
+
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
+
+export default App
